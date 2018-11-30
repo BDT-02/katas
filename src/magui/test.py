58a99c2dd9aa5d26b6676ebbf5b2eb907be27c9e
mfuentes-ME <magali.fa@gmail.com>
2018-11-30 13:30:24 -0400

    task first test

--- src/magui/test.py
+++ src/magui/test.py
@@ -1,1 +1,52 @@
 print "hello"
+a = 4
+b = 5
+
+print "la suma es:" + str(a + b)
+print a - b
+print a / b
+print a % b
+print a ** b
+print a//b
+
+if a == b:
+    print True
+else:
+    print False
+
+a += b
+print a
+a -= a
+print a
+
+b *= a
+print b
+
+c = 10
+d = 2
+c /= d
+print c
+
+#a %= b
+
+#print a
+
+l1 = 1
+l2 = 2
+l3 = a
+list = [1,2,4,6,3]
+list1 = [a,b,c]
+if(l1 in list):
+    print "L1 is in list"
+else:
+    print ("L1 not exist in the list")
+if (l3 not in list1 ):
+    print "l2 not in the list"
+else:
+    print"L2 is in the list"
+
+value1 = 2
+value2 = 3
+
+if (value1 is value2):
+    print "are equals"
