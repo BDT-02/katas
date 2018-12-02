98d75b334db2a6acfa02973cced891e8e6c8df87
edgartron32@gmail.com <edgartron32@gmail.com>
2018-12-02 14:17:56 -0400

    lesson 2 area circle and total sum of integer.

--- src/edgarriver/lesson2PrimeNumber.py
+++ src/edgarriver/lesson2PrimeNumber.py
@@ -1,19 +1,8 @@
-def get_prime_numbers(max_number):
-    numbers = [True, True] + [True] * (max_number - 1)
-    last_prime_number = 2
-    i = last_prime_number
-    while last_prime_number ** 2 <= max_number:
-        i += last_prime_number
-        while i <= max_number:
-            numbers[i] = False
-            i += last_prime_number
-        j = last_prime_number + 1
-        while j < max_number:
-            if numbers[j]:
-                last_prime_number = j
-                break
-            j += 1
-        i = last_prime_number
-    print [i + 2 for i, not_crossed in enumerate(numbers[2:]) if not_crossed]
+def get_prime_numbers(min_number,max_number):
+    for num in range(min_number, max_number):
+        if 0 not in [num % i for i in range(2, int(num / 2) + 1)]:
+            print(str(num) + " prime")
+        else:
+            print(str(num) + " not prime")
+get_prime_numbers(int(raw_input("Enter a number: ")), int(raw_input("Enter a number: ")))
 
-get_prime_numbers(45)
