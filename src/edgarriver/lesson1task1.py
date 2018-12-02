6739869f312446cd265345f7b8ba6e1da500772d
edgartron32@gmail.com <edgartron32@gmail.com>
2018-12-02 00:27:19 -0400

    task1

--- src/edgarriver/lesson1task1.py
+++ src/edgarriver/lesson1task1.py
@@ -12,8 +12,8 @@ def taskOperators(op,num1,num2):
     if op == "%":
         result = num1 % num2
     print(num1, op, num2, "=", result)
-numero1 = int(input("Ingrese primer numero: "))
-numero2 = int(input("Ingrese segundo numero: "))
+numero1 = int(input("Ingrese primer numeros: "))
+numero2 = int(input("Ingrese segundo numeros: "))
 taskOperators("+", numero1, numero2)
 taskOperators("-", numero1, numero2)
 taskOperators("*", numero1, numero2)
