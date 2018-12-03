73e464f98572ff6a0ed40fa9c6c391f637199b04
magnethus <magnethus@gmail.com>
2018-12-03 09:10:47 -0400

    lastchange2

---
+++ src/alex/PPT2.py
@@ -0,0 +1,16 @@
+
+#PRACTICE
+# 1. Create 1 script to determine is a number is odd or even (use single line statement if applies)
+# 2. According a list of values between a Min and Max range, identify if the number is prime or not.
+
+#EXERCISES
+
+#Write a function area_of_circle(r) which returns the area of a circle of radius r only if the radius value is greater of 10.
+#Write a function sum_to(n) that returns the sum of all integer numbers up to and including only until any value lower than 35.
+##     So sum_to(10)wouldbe1+2+3...+10which would return the value 55, but if n=40 only until sum to 35 need to be returned.
+#Write a function days_in_month which takes the name of a month, and returns the number of days in the month. Ignore leap years:
+#		days_in_month("February") == 28 				    days_in_month("December") == 31
+#       If the function is given invalid arguments, it should return None.
+
+
+
