4bba0b04a8da82263fc537d7a442b96cfc325859
vzamorano <vivianzamoranocastelo58@gmail.com>
2018-12-03 08:51:19 -0400

    Test

---
+++ src/vivian/Class1.py
@@ -0,0 +1,29 @@
+#Class 1
+#Practice 2
+a=20
+b=2
+
+#Arithmetic's operators
+print a+b
+print a-b
+print a*b
+print a/b
+print a**b
+print a//b
+
+#Comparison operators
+print a>b
+print a!=b
+print a==b
+print b<a
+print a>=b
+print b<=a
+
+#Assignment operators
+c=a+b
+print c
+print c+=a
+print c-=a
+print c*=a
+print c/=a
+print c%=a
