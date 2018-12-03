3b43b7bc2352c700971f7b59307593a3b1c449e9
vzamorano <vivianzamoranocastelo58@gmail.com>
2018-12-03 09:26:22 -0400

    Updated Class1

--- src/vivian/Class1.py
+++ src/vivian/Class1.py
@@ -1,7 +1,15 @@
 #Class 1
+#Practice 1
+def perform_operation(n1,n2,op):
+    operations={"+": n1+n2, "*": n1*n2, "/": n1/n2, "%": n1%n2,"**": n1**n2, "//": n1//n2}
+    print ("The result is: ", operations[op])
+perform_operation(2,5,"**")
+
+
 #Practice 2
 a=20
 b=2
+c=10
 
 #Arithmetic's operators
 print a+b
@@ -20,10 +28,13 @@ print a>=b
 print b<=a
 
 #Assignment operators
-c=a+b
+c+=a
 print c
-print c+=a
-print c-=a
-print c*=a
-print c/=a
-print c%=a
+c-=a
+print c
+c*=a
+print c
+c/=a
+print c
+c%=a
+print c
