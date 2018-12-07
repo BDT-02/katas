9cc63dc9111c22d883bba91ddd74b791d00cda6d
vzamorano <vivianzamoranocastelo58@gmail.com>
2018-12-07 09:11:54 -0400

    Class5

--- src/vivian/Class2.py
+++ src/vivian/Class2.py
@@ -1,10 +1,30 @@
-#Practice 1
+#PRACTICE 1
 #Exercise 1
-num = 15
-if (num == 15):
-    print ("The number %d is impar" %(num))
-else:
-    print ("The number %d is not par" % (num))
+import math
+def number(n):
+    if (n%2 == 0):
+        print ("Even number")
+    else:
+        print ("Odd number")
+number(10)
 
 #Exercise 2
-num
+def prime(a):
+    c=0
+    n=1
+    while n<a:
+        if ((a%n) == 0):
+            c+=1
+        n+=1
+    if (c==1):
+        print(a, "Prime number")
+    else:
+        print(a, "Not a prime number")
+
+def numb(min,max,numb):
+    for value in numb:
+        if (value>min and value<max ):
+            prime(value)
+min=2
+max=30
+numb(min,max,numb)
